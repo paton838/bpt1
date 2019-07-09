8a2a18ab55ce85c27abc9ca525f2a1a5ecb29571
ashz30 <ashz30@gmail.com>
2019-07-09 15:07:46 +0530

    anged

--- release 1/processes/processdeploy.bat
+++ release 1/processes/processdeploy.bat
@@ -1,1 +1,1 @@
-"C:\Program Files\Blue Prism Limited\Blue Prism Automate\AutomateC.exe" /import "BPA Process - First Process.xml" /overwrite /user admin admin /dbconname 6.5
+"C:\Program Files\Blue Prism Limited\Blue Prism Automate\AutomateC.exe" /import "BPA Process - First Process.xml" /overwrite /user %1 %2 /dbconname %3
--- release 1/releases/releasedeploy.bat
+++ release 1/releases/releasedeploy.bat
@@ -1,1 +1,1 @@
-"C:\Program Files\Blue Prism Limited\Blue Prism Automate\AutomateC.exe" /importrelease TestGit.bprelease /overwrite /user admin admin /dbconname 6.5
+"C:\Program Files\Blue Prism Limited\Blue Prism Automate\AutomateC.exe" /importrelease TestGit.bprelease /overwrite /user %1 %2 /dbconname %3
