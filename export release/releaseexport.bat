bc9b3522eb3a9c2f6f84c39ac15e61ec8ca7d0cd
ashz30 <ashz30@gmail.com>
2020-06-25 15:09:22 +0530

    added export release

--- export release/releaseexport.bat
+++ export release/releaseexport.bat
@@ -1,1 +1,1 @@
-"C:\Program Files\Blue Prism Limited\Blue Prism Automate\AutomateC.exe" /exportpackage TestExport /user admin admin /dbconname jenkins
+"C:\Program Files\Blue Prism Limited\Blue Prism Automate\AutomateC.exe" /exportpackage TestExport /user admin admin /dbconname jenkins >>output.txt
