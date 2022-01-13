0660838155daf5f311980acfbc0e1a3a7a1df5d8
paton838 <39183802+paton838@users.noreply.github.com>
2022-01-13 13:55:45 +1300

    Added by Jenkins to master for auto export

--- release 9/releasedeploy.bat
+++ release 9/releasedeploy.bat
@@ -1,4 +1,1 @@
-"C:\Program Files\Blue Prism Limited\Blue Prism Automate\AutomateC.exe" /importrelease TestGit.bprelease /overwrite /user %1 %2 /dbconname %3
-
-"C:\Program Files\Blue Prism Limited\Blue Prism Automate\AutomateC.exe" /importrelease "Registration Process.bprelease" /overwrite /user %1 %2 /dbconname %3
-
+"C:\Program Files\Blue Prism Limited\Blue Prism Automate\AutomateC.exe" /importrelease %4 /overwrite /user %1 %2 /dbconname %3
